bf76d841947e4a31016809a0a83444a66f65d7f5
hikakankan <furuta@mxb.mesh.ne.jp>
2016-03-11 17:50:41 +0900

    サーバー側ファイルに変換

---
+++ ServerConverter/ServerConverter/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ServerConverter
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            conversions.Add(@"C:\furuta\repository\TransLiner\TransLinerTS\TransLiner\TLPage.js", @"C:\furuta\repository\TransLiner\TransLinerTS\TransLiner\TLPageServer.js");
+            conversions.Add(@"C:\furuta\repository\TransLiner\TransLinerTS\TransLiner\TLRootPage.js", @"C:\furuta\repository\TransLiner\TransLinerTS\TransLiner\TLRootPageServer.js");
+            conversions.Add(@"C:\furuta\repository\TransLiner\TransLinerTS\TransLiner\TLPageSettings.js", @"C:\furuta\repository\TransLiner\TransLinerTS\TransLiner\TLPageSettingsServer.js");
+        }
+
+        private Conversions conversions = new Conversions();
+
+        private void buttonConvert_Click(object sender, RoutedEventArgs e)
+        {
+            conversions.Convert();
+            System.Windows.MessageBox.Show("終了");
+        }
+    }
+}
